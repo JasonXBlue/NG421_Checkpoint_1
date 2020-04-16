3d4961c567bded500c23b85fc991e0bf0bb8704d
JasonXBlue <JasonX.blue1@gmail.com>
2020-04-15 19:00:08 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { CreateTodoComponent } from './create-todo/create-todo.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
-import {RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { TodoComponent } from "./todo/todo.component";
+import { TodoListComponent } from "./todo-list/todo-list.component";
+import { CreateTodoComponent } from "./create-todo/create-todo.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ConfirmationModalComponent } from "./confirmation-modal/confirmation-modal.component";
+import { RouterModule } from "@angular/router";
 
 const routes = [];
 const appRoutes = RouterModule.forRoot(routes);
@@ -18,16 +18,11 @@ const appRoutes = RouterModule.forRoot(routes);
     TodoComponent,
     TodoListComponent,
     CreateTodoComponent,
-    ConfirmationModalComponent
+    ConfirmationModalComponent,
   ],
-  imports: [
-    BrowserModule,
-    appRoutes,
-    FormsModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, appRoutes, FormsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: []
+  entryComponents: [TodoComponent, ConfirmationModalComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,25 +1,24 @@
-import { Injectable } from '@angular/core';
-import {ITodo} from '../interfaces/itodo';
+import { Injectable } from "@angular/core";
+import { ITodo } from "../interfaces/itodo";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TodoService {
   todoId: number = 0;
-  todoList: ITodo [] = [
+  todoList: ITodo[] = [
     // example of how to make an item in todo list
-    { title: 'Install Angular CLI', id: this.todoId },
-  
-  ]
-  constructor() { }
-  getTodos(){
+    { title: "Install Angular CLI", id: this.todoId },
+  ];
+  constructor() {}
+  getTodos() {
     return this.todoList;
   }
   deleteTodo(todo: ITodo) {
-    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    const index = this.todoList.findIndex((todoItem) => todoItem === todo);
     this.todoList.splice(index, 1);
   }
-  addTodo(todo: ITodo):void {
-    todo.id = this.todoId ++;
-    todoList.push(todo);
+  addTodo(todo: ITodo): void {
+    todo.id = this.todoId++;
+    this.todoList.push(todo);
   }
 }
